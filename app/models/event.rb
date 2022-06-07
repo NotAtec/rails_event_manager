ba929e9812f3a1d4004e6eca741c17cc2eae4bf3
Jeroen B <jeroenb04@live.com>
2022-06-07 13:22:10 +0200

    Event Validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :host, class_name: 'User'
+
+  validates :location, presence: true
+  validates :date, presence: true
+  validates :date, format: { with: /\A\d{4}-\d{2}-\d{2}\z/, message: "must be in the format of YYYY-MM-DD" }
 end
