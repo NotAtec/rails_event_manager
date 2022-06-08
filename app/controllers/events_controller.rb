1b97a1c0bc19e6c32745670bffd17802dda50279
Jeroen B <jeroenb04@live.com>
2022-06-08 22:39:34 +0200

    Event scaffold related page design
    Design for _event and _form partials
    Design for edit, index, new & show pages
    Updates to database where required

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -66,6 +66,6 @@ class EventsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def event_params
-      params.require(:event).permit(:date, :location)
+      params.require(:event).permit(:title, :description, :date, :location)
     end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -3,5 +3,5 @@ class Event < ApplicationRecord
 
   validates :location, presence: true
   validates :date, presence: true
-  validates :date, format: { with: /\A\d{4}-\d{2}-\d{2}\z/, message: "must be in the format of YYYY-MM-DD" }
+  validates :date, format: { with: /\A\d{4}-\d{2}-\d{2}\z/, message: "This must be in the format of YYYY-MM-DD" }
 end
