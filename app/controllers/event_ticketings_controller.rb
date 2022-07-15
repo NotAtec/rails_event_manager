9eb0b9cb4b184e42b40c61ffd788a3db3c5a466b
Jeroen B <jeroenb04@live.com>
2022-07-15 23:50:11 +0200

    Added routing logic & fixed bug
    Fixed bug with event-ticketing not working properly due to naming.
    Added routing for creation and destruction of tickets

--- app/controllers/event_ticketings_controller.rb
+++ app/controllers/event_ticketings_controller.rb
@@ -1,4 +1,4 @@
-class EventTicketingController < ApplicationController
+class EventTicketingsController < ApplicationController
   before_action :find_event
   before_action :find_user
 
@@ -9,6 +9,7 @@ class EventTicketingController < ApplicationController
       @event.attendees.push(@user)
       flash[:notice] = "Succesfully added ticket!"
     end
+    redirect_to event_path(@event)
   end
 
   def destroy
@@ -18,12 +19,13 @@ class EventTicketingController < ApplicationController
     else
       flash[:notice] = "You don't have a ticket!"
     end
+    redirect_to event_path(@event)
   end
 
   private
 
   def find_event
-    @event = Event.find(params[:event_id])
+    @event = params[:id].nil? ? Event.find(params[:format]) : Event.find(params[:id])
   end
 
   def find_user
@@ -31,6 +33,6 @@ class EventTicketingController < ApplicationController
   end
 
   def already_going?
-    EventTicketing.where(event_attendee_id: current_user.id, attended_event_id: params[:event_id]).exists?
+    EventTicketing.where(event_attendee_id: current_user.id, attended_event_id: params[:id]).exists?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :events
   resources :users, only: [:index, :show]
-
+  resources :event_ticketings, only: [:create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
